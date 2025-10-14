770ec1685554afccdb86cbca63fe3066386d1025
Oko-Osi Korede <okoosikorede@gmail.com>
2025-10-14 23:39:48 +0100

    Initial Commit

---
+++ LincRideAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LincRideAssessment
+//
+//  Created by Oko-Osi Korede on 14/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
